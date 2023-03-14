63d5f79fc7a36438ab90b1fba05539f62e041fc4
pt <2681247670@qq.com>
2023-03-14 22:13:10 +0800

    引入element-plus

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import ElementUI from "element-ui";
-import "element-ui/lib/theme-chalk/index.css";
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 const app = createApp(App)
-app.use(ElementUI)
+app.use(ElementPlus)
 
 app.use(store).use(router).mount("#app");
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,5 +1,14 @@
 <template>
-  <div id="login">我是登录页面</div>
+  <div id="login">我是登录页面
+
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </div>
+  
 </template>
 <script>
 export default{
